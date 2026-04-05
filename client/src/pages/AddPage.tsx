fea4c442a0b6a71ca28f0b2538f846be15ccf4e0
Hadasalon10 <hadasal0n20@gmail.com>
2026-04-06 01:24:47 +0300

    create carf for add page

--- client/src/pages/AddPage.tsx
+++ client/src/pages/AddPage.tsx
@@ -8,34 +8,35 @@ export default function AddPage() {
 
   return (
     <div className="add-options-page">
-      <h1 className="page-title">Add Recipe</h1>
+       <div className="add-page-container">
+        <h1 className="page-title">Add Recipe</h1>
 
-      <div className="options-container">
+        <div className="options-container">
 
-        <div
-          className="option-card"
-          onClick={() => navigate("/createRecipe")}
-        >
-          <div className="option-icon">➕</div>
-          <div>
-            <h2>Add Recipe</h2>
-            <p>Create your own recipe</p>
+          <div
+            className="option-card"
+            onClick={() => navigate("/createRecipe")}
+          >
+            <div className="option-icon">➕</div>
+            <div>
+              <h2>Add Recipe</h2>
+              <p>Create your own recipe</p>
+            </div>
           </div>
-        </div>
 
-        <div
-          className="option-card"
-          onClick={() => navigate("/createRecipeBook")}
-        >
-          <div className="option-icon">📖</div>
-          <div>
-            <h2>Add Book Recipe</h2>
-            <p>Add a recipe to a recipe book</p>
+          <div
+            className="option-card"
+            onClick={() => navigate("/createRecipeBook")}
+          >
+            <div className="option-icon">📖</div>
+            <div>
+              <h2>Add Book Recipe</h2>
+              <p>Add a recipe to a recipe book</p>
+            </div>
           </div>
-        </div>
 
+        </div>
       </div>
-
       <BottomNav />
     </div>
   );
